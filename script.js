cad132796552fa653833d9de36f8aa8bad155545
shyamdharanm <shyamdharan214@gmail.com>
2023-04-15 11:04:12 +0530

    s

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+
+const myForm = document.getElementById("myForm");
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstName = document.getElementById("firstName").value;
+  const middleName = document.getElementById("middleName").value;
+  const lastName = document.getElementById("lastName").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  console.log(`First Name: ${firstName}`);
+  console.log(`Middle Name: ${middleName}`);
+  console.log(`Last Name: ${lastName}`);
+  console.log(`Email: ${email}`);
+  console.log(`Password: ${password}`);
+});
