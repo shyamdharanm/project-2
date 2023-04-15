cb7d2c964115daf339d49e38c41d5dd08fa1d2c6
shyamdharanm <shyamdharan214@gmail.com>
2023-04-15 11:14:14 +0530

    a

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 
-const myForm = document.getElementById("myForm");
+const form = document.createElement("form");
 
-myForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const firstName = document.getElementById("firstName").value;
-  const middleName = document.getElementById("middleName").value;
-  const lastName = document.getElementById("lastName").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  console.log(`First Name: ${firstName}`);
-  console.log(`Middle Name: ${middleName}`);
-  console.log(`Last Name: ${lastName}`);
-  console.log(`Email: ${email}`);
-  console.log(`Password: ${password}`);
-});
+// Create label and input elements for first name
+var firstNameLabel = document.createElement("label");
+firstNameLabel.textContent = "First Name:";
+var firstNameInput = document.createElement("input");
+firstNameInput.type = "text";
+firstNameInput.name = "firstname";
+
+// Create label and input elements for middle name
+var middleNameLabel = document.createElement("label");
+middleNameLabel.textContent = "Middle Name:";
+var middleNameInput = document.createElement("input");
+middleNameInput.type = "text";
+middleNameInput.name = "middlename";
+
+// Create label and input elements for last name
+var lastNameLabel = document.createElement("label");
+lastNameLabel.textContent = "Last Name:";
+var lastNameInput = document.createElement("input");
+lastNameInput.type = "text";
+lastNameInput.name = "lastname";
+
+var emailLabel = document.createElement("label");
+emailLabel.textContent = "Email:";
+var emailInput = document.createElement("input");
+emailInput.type = "email";
+emailInput.name = "email";
+
+// Create label and input elements for password
+var passwordLabel = document.createElement("label");
+passwordLabel.textContent = "Password:";
+var passwordInput = document.createElement("input");
+passwordInput.type = "password";
+passwordInput.name = "password";
+
+// Append the label and input elements to the form element
+form.appendChild(firstNameLabel);
+form.appendChild(firstNameInput);
+form.appendChild(middleNameLabel);
+form.appendChild(middleNameInput);
+form.appendChild(lastNameLabel);
+form.appendChild(lastNameInput);
+form.appendChild(emailLabel);
+form.appendChild(emailInput);
+form.appendChild(passwordLabel);
+form.appendChild(passwordInput);
+
+// Append the form element to the document body
+document.body.appendChild(form);
