ff4942aa03aebf3e29bcde56975cd9bd90bbbf73
shyamdharanm <shyamdharan214@gmail.com>
2023-04-15 12:53:15 +0530

    s

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-var button=document.createElement("button");
-var span=document.createElement("span");
-button.setAttribute("type","button");
-span.setAttribute("type","span");
-button.addEventListener("click",foo);
-document.body.append(button);
-function foo(){
-    alert(parseInt(prompt("enter your age")));
-}
+var name="learning";
+var msg="javascript" + name + "session";
+alert(msg);
+console.log(msg);
