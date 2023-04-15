6e6d16337da92d7e86444c2719906d8b9aad797e
shyamdharanm <shyamdharan214@gmail.com>
2023-04-15 12:18:11 +0530

    d

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var span=document.createElement("span");
 button.setAttribute("type","button");
 span.setAttribute("type","span");
 button.addEventListener("click",foo);
-span.textContent=value;
 document.body.append(button);
 function foo(){
     alert(parseInt(prompt("enter your age")));
