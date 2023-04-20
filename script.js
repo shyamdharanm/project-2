ab50bbc7e5213b4f6341e179e4c9f650da472217
shyamdharanm <shyamdharan214@gmail.com>
2023-04-20 12:46:46 +0530

    sa

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function countChars(obj){
-    document.getElementById("charNum").innerHTML = obj.value.length+' characters';
+// Initialize variables
+let result = "";
+let operator = "";
+
+// Get DOM elements
+const resultElement = document.getElementById("result");
+
+// Function to add input to the result
+function addToResult(input) {
+  result += input;
+  resultElement.value = result;
+}
+
+// Function to calculate the result
+function calculateResult() {
+  result = eval(result);
+  resultElement.value = result;
 }
