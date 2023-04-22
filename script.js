d35334d0f4076824bb00d24906d6c6091909811d
shyamdharanm <shyamdharan214@gmail.com>
2023-04-22 12:34:47 +0530

    s

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-x=10;
-document.write(x);
-var x;
+const form = document.getElementById('content');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting
+  
+  const fname = document.getElementById('fname').value;
+  const lname = document.getElementById('lname').value;
+  const name=fname + '' + lname
+
+  const address = document.getElementById('address').value;
+
+
+
+
+  console.log(`Name: ${name}`);
+
+  //to loca strg
+
+localStorage.setItem("name",name)
+localStorage.setItem("address",address)
+
+});
