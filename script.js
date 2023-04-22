09d64a5486f667b9ac04b9b8e2da648ba4a90e35
shyamdharanm <shyamdharan214@gmail.com>
2023-04-22 11:00:18 +0530

    s

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-// Initialize variables
-let result = "";
-let operator = "";
-
-// Get DOM elements
-const resultElement = document.getElementById("result");
-
-// Function to add input to the result
-function addToResult(input) {
-  result += input;
-  resultElement.value = result;
-}
-
-// Function to calculate the result
-function calculateResult() {
-  result = eval(result);
-  resultElement.value = result;
-}
+var heading=document.querySelector(p);
+console.log(e);
